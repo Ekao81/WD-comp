09ee58d57738e20e13aff30cd6a8372f37d0f8c3
Ekao81 <109479525+Ekao81@users.noreply.github.com>
2024-02-12 18:37:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    let params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message: document.getElementById("message").value,
+
+    }
+        emailjs.send("service_cbd093i", "template_7viayie",params).then(alert("Email sent!"))
+}
